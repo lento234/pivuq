ba3b50da7b5ba427545ee11612c8e0a16aa36cc1
MrLento234 <lento.manickathan@gmail.com>
2022-04-10 22:43:32 +0200

    first implementation of pixel-level warping (#3)

---
+++ src/pivuq/warping.py
@@ -0,0 +1,59 @@
+import numpy as np
+import scipy.interpolate as spinterp
+import skimage
+
+
+def warp_skimage(I, U, coords, order=3, mode="edge"):
+    row_coords, col_coords = coords
+    # Warp
+    u, v = U
+
+    warped_frame = skimage.transform.warp(
+        I, np.array([row_coords - v, col_coords - u]), order=order, mode=mode
+    )
+
+    return warped_frame
+
+
+def interpolate_to_pixel(U, imshape, kind="linear"):
+
+    # Velocity components
+    u, v = U
+    nr, nc = u.shape
+
+    ws = imshape[0] / nr
+
+    x, y = np.arange(nr) * ws + ws / 2, np.arange(nc) * ws + ws / 2
+    xi, yi = np.arange(imshape[0]), np.arange(imshape[1])
+
+    # Interpolate to pixel level
+    u_px = spinterp.interp2d(x, y, u, kind=kind)(xi, yi)
+    v_px = spinterp.interp2d(x, y, v, kind=kind)(xi, yi)
+
+    return np.stack((u_px, v_px))
+
+
+def warp(image_pair, U, direction="center", upsample_kind="linear", order=1, nsteps=5):
+
+    # warping image pairs
+    I1hat, I2hat = image_pair
+    nr, nc = I1hat.shape
+
+    # interpolate velocity to pixel level
+    U_interp = interpolate_to_pixel(U, (nr, nc), kind=upsample_kind)
+    U_substep = U_interp / nsteps
+
+    # generate mapping grid
+    image_coords = np.meshgrid(np.arange(nr), np.arange(nc), indexing="ij")
+
+    # warp images in nsteps
+    for istep in range(nsteps):
+        if direction == "forward":
+            I1hat = warp_skimage(I1hat, U_substep, image_coords, order=order)
+        elif direction == "backward":
+            I2hat = warp_skimage(I2hat, -U_substep, image_coords, order=order)
+        elif direction == "center":
+            I1hat = warp_skimage(I1hat, 0.5 * U_substep, image_coords, order=order)
+            I2hat = warp_skimage(I2hat, -0.5 * U_substep, image_coords, order=order)
+
+    return np.stack((I1hat, I2hat))
