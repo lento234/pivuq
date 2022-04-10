6b992b1c8eac53110a834906fb61142a9c434b3e
MrLento234 <lento.manickathan@gmail.com>
2022-04-10 18:50:00 +0200

    renamed test, explicit version check

--- tests/test_import.py
+++
@@ -1,7 +0,0 @@
-from importlib import metadata
-
-
-def test_import():
-    import pivuq
-
-    assert pivuq.__version__ == metadata.version("pivuq")
---
+++ tests/test_version.py
@@ -0,0 +1,4 @@
+
+def test_version():
+    import pivuq
+    assert pivuq.__version__ == "0.1.0"
